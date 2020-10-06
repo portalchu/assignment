e82ef586d54833e77eaf0c0490d852263210469f
portalchu <56888404+portalchu@users.noreply.github.com>
2020-10-07 00:15:05 +0900

    Add files via upload

---
+++ 5주차 과제란/2번 문제.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+class Something
+{
+public:
+	int m_value;
+	Something() : m_value(0) {}
+	void setValue(int value) { m_value = value; }
+
+	void resetValue() { m_value = 0; }
+	int getValue () const { return m_value; }
+};
+
+void printValue(const Something& sData) {
+	cout << "value :" << sData.getValue() << endl;
+}
+
+void a(int& n) {
+	n = 3;
+}
+
+int main(int argc, char const* argv[])
+{
+	Something first;
+
+	first.setValue(3);
+	cout << first.getValue() << endl;
+	first.resetValue();
+	printValue(first);
+
+	return 0;
+}
